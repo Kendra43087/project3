737a758e6dc4e89877f0fe62019a5339accc22d8
Kendra43087 <38871916+Kendra43087@users.noreply.github.com>
2018-10-10 00:23:26 -0400

    Add files via upload

---
+++ client/src/src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Card from './components/card.js';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+
+
+
+        <header>
+          <h1 className="App-title">Mom's Intuition</h1>
+        </header>
+
+
+
+
+       <p className="App-intro">
+           Rare Childhood Disibalities </p>
+
+           
+           
+
+        <div className="cardContainer">
+
+
+        <button class="button button2">Hallerman Straff Syndrome</button>
+        <button class="button button2">Apert Sydrome</button>
+        <button class="button button2">Myelomeningocde</button>
+        <button class="button button2">Rett Sydrome</button>
+        <button class="button button2">Diprosopus</button>
+        <button class="button button2">Ostegensis Imperfecta</button>
+        <button class="button button2">Acute Flaccid Myelitis</button>
+        <button class="button button2">Treaher Collins Syndrome</button>
+        <button class="button button2">Bixler Syndrome</button>
+        <button class="button button2">Juvenile HungtingtonsTreaher Collins Syndrome</button>
+        <button class="button button2">Neourofiberomat-osis</button>
+        <button class="button button2">Goldenher Syndromecute Flaccid Myelitis</button>
+        <button class="button button2">Candle Syndrome</button>
+        <button class="button button2">Nager Syndrome</button>
+        <button class="button button2">Nemaline Myopathy</button>
+        <button class="button button2">Pfeiffer Syndrome</button>
+      
+       
+   
+
+      
+      <img src="./images/background.jpg" alt="" width="100%" height="auto"/>
+
+       </div>
+
+    
+       </div>
+
+        
+   
+
+   );
+
+        <button class="button button2" app src="./App.js">submit</button>
+
+  }
+}
+
+
+export default App;
+
