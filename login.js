6ce41c8de62e58f16bd77a95cd40770cbceee8e3
Kendra43087 <38871916+Kendra43087@users.noreply.github.com>
2018-10-10 00:21:56 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './login.css';
+
+class Login extends Component {  
+    render (){
+        // var uname = document.forms["myForm"]["username"].value;
+        // var pass = document.forms["myForm"]["password"].value;
+       return 
+        <div className="Login" >
+        
+        <header>
+          <h1 className="Login-title">Mom's Intuition</h1>
+        </header>
+
+
+        <div className="loginContainer">
+
+         
+         
+         
+         
+      
+   </div>
+   </div>
+    }
+    
+    
+}
+
+
+
+
+
+         <button class="button button2" app src="./App.js">submit</button>
+  
+   
+    
+
+export default Login;
